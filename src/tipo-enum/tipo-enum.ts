89865b1ed784388745c04484b589878fd2f9f520
vinicius Fernandes <vinicim.fernandez1@gmail.com>
2021-01-29 18:45:12 -0300

    tipo enum

---
+++ src/tipo-enum/tipo-enum.ts
@@ -0,0 +1,17 @@
+enum Cores {
+    Vermelho,//0
+    azul,//1
+    amarelo,//2
+    roxo = 'Roxo',//Roxo
+    verde = 4 //ele não consegue continuar a contagem automática porcausa do roxo
+}
+
+// function escolhaAcor(cor : Cores) : void {
+//     console.log(Cores[cor]);
+// }
+
+// // console.log
+// // (Cores)
+// // console.log(Cores.Vermelho);
+// // console.log(Cores[0]);
+// escolhaAcor(0)
